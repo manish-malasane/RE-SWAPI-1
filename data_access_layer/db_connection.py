a99661ce9b85b6b11eadd067814ce761fd593a77
manish <mmalasane6@gmail.com>
2024-04-16 17:43:27 +0530

    yaml & toml db credential connection successful

--- data_access_layer/db_connection.py
+++ data_access_layer/db_connection.py
@@ -1,15 +1,26 @@
 import pymysql
 import toml
-from pymysql import connect, cursors
+from pymysql import connect  # cursors
+import yaml
 
 
 def get_toml_conn():
     try:
         with open('data_access_layer/settings/secrets.toml') as fp:
-            config = toml.load(fp)
-            db_config = config.get('mysqldb')
-            connection = connect(**db_config)
-            return connection
+            toml_config = toml.load(fp)
+            toml_db_config = toml_config.get('mysqldb')
+            t_connection = connect(**toml_db_config)
+            return t_connection
+    except pymysql.Error as ex:
+        print(f"[ ERROR ] connection cannot successful - {ex}")
+
+
+def get_yaml_conn():
+    try:
+        with open("data_access_layer/settings/secrets.yaml") as fp:
+            yaml_config = yaml.load(fp, Loader=yaml.FullLoader)
+            y_connection = connect(**yaml_config)
+            return y_connection
     except pymysql.Error as ex:
         print(f"[ ERROR ] connection cannot successful - {ex}")
 
@@ -24,8 +35,9 @@ def get_toml_conn():
 #                              cursorclass=cursors.DictCursor)
 #         return connection
 #     except pymysql.Error as ex:
-#         print(f"[ ERROR ] connection cannot successfull - {ex}")
+#         print(f"[ ERROR ] connection cannot successful - {ex}")
 
 if __name__ == "__main__":
-    get_toml_conn()
+    toml_conn = get_toml_conn()
+    yaml_conn = get_yaml_conn()
     breakpoint()
