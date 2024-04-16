57297d913bd840abfe7bee4e426f27ee09db2288
manish <mmalasane6@gmail.com>
2024-04-16 16:49:52 +0530

    toml connectiion

--- data_access_layer/db_connection.py
+++ data_access_layer/db_connection.py
@@ -3,26 +3,29 @@ import toml
 from pymysql import connect, cursors
 
 
-def make_connection():
-    try:
-        connection = connect(host='localhost',
-                             user='root',
-                             password='Aaibaba',
-                             port=3306,
-                             database='PRACTISE',
-                             cursorclass=cursors.DictCursor)
-        return connection
-    except pymysql.Error as ex:
-        print(f"[ ERROR ] connection cannot successfull - {ex}")
-
-
 def get_toml_conn():
-    with open('data_access_layer/settings/secrets.toml') as fp:
-        config = toml.load(fp)
-        print(config)
+    try:
+        with open('data_access_layer/settings/secrets.toml') as fp:
+            config = toml.load(fp)
+            db_config = config.get('mysqldb')
+            connection = connect(**db_config)
+            return connection
+    except pymysql.Error as ex:
+        print(f"[ ERROR ] connection cannot successful - {ex}")
 
+
+# def make_connection():
+#     try:
+#         connection = connect(host='localhost',
+#                              user='root',
+#                              password='Aaibaba',
+#                              port=3306,
+#                              database='PRACTISE',
+#                              cursorclass=cursors.DictCursor)
+#         return connection
+#     except pymysql.Error as ex:
+#         print(f"[ ERROR ] connection cannot successfull - {ex}")
 
 if __name__ == "__main__":
-    conn = make_connection()
     get_toml_conn()
     breakpoint()
